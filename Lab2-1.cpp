03d3155ee07df6bb5af42c8348adf2a91968b7b7
Jef Dewitt <jef.dewitt@twinspires.com>
2020-05-13 13:49:40 -0400

    Finished project files.

---
+++ Lab2-1.cpp
@@ -0,0 +1,169 @@
+//============================================================================
+// Name        : Lab2-1.cpp
+// Author      : Jef DeWitt
+// Version     : 1.0
+// Copyright   : Copyright © 2017 SNHU COCE
+// Description : Lab 2-1 Up to Speed in C++, Part 2
+//============================================================================
+
+#include <algorithm>
+#include <iostream>
+#include <time.h>
+
+#include "CSVparser.hpp" // Include CSVParser third-party code
+
+using namespace std;
+
+//============================================================================
+// Global definitions visible to all methods and classes
+//============================================================================
+
+// forward declarations
+double strToDouble(string str, char ch);
+
+struct Bid {
+    string title;
+    string fund;
+    double amount;
+    Bid() {
+        amount = 0.0;
+    }
+};
+
+//============================================================================
+// Static methods used for testing
+//============================================================================
+
+/**
+ * Simple C function to convert a string to a double
+ * after stripping out unwanted char
+ *
+ * credit: http://stackoverflow.com/a/24875936
+ *
+ * @param ch The character to strip out
+ */
+double strToDouble(string str, char ch) {
+    str.erase(remove(str.begin(), str.end(), ch), str.end());
+    return atof(str.c_str());
+}
+
+/**
+ * Display the bid information
+ *
+ * @param bid struct containing the bid info
+ */
+void displayBid(Bid bid) {
+    cout << bid.title << " | " << bid.amount << " | " << bid.fund << endl;
+    return;
+}
+
+/**
+ * Prompt user for bid information
+ *
+ * @return Bid struct containing the bid info
+ */
+Bid getBid() {
+    Bid bid;
+
+    cout << "Enter title: ";
+    cin.ignore();
+    getline(cin, bid.title);
+
+    cout << "Enter fund: ";
+    cin >> bid.fund;
+
+    cout << "Enter amount: ";
+    cin.ignore();
+    string strAmount;
+    getline(cin, strAmount);
+    bid.amount = strToDouble(strAmount, '$');
+
+    return bid;
+}
+
+/**
+ * Load a CSV file containing bids into a container
+ *
+ * @param csvPath the path to the CSV file to load
+ * @return a container holding all the bids read
+ */
+vector<Bid> loadBids(string csvPath) {
+	vector<Bid> bidCollection; // Create a data structure to hold data from each row and add to vector
+
+    // initialize the CSV Parser using the given path
+    csv::Parser file = csv::Parser(csvPath);
+
+	// loop to read rows of a CSV file
+	for (int i = 0; i < file.rowCount(); i++) {
+
+	    // File columns: ArticleTitle, ArticleID, Department, CloseDate, WinningBid, InventoryID, VehicleID, ReceiptNumber, Fund
+
+        Bid bid; // Create a Bid instance to structure data coming in from file
+        bid.title = file[i][0];
+        bid.fund = file[i][8];
+        bid.amount = strToDouble(file[i][4], '$');
+        bidCollection.push_back(bid); // Add bid to vector
+
+    }
+
+    return bidCollection; // Return the collection of Bids back to whoever calls this method
+}
+
+/**
+ * The one and only main() method
+ */
+int main(int argc, char* argv[]) {
+
+    // process command line arguments
+    string csvPath;
+    switch (argc) {
+    case 2:
+        csvPath = argv[1];
+        break;
+    default:
+        csvPath = "eBid_Monthly_Sales_Dec_2016.csv";
+    }
+
+    vector<Bid> bids; // Define a vector to hold all the bids
+
+    clock_t ticks; // Define a timer variable
+
+    int choice = 0;
+    while (choice != 9) {
+        cout << "Menu:" << endl;
+        cout << "  1. Enter a Bid" << endl;
+        cout << "  2. Load Bids" << endl;
+        cout << "  3. Display All Bids" << endl;
+        cout << "  9. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+        	cout << "Not currently implemented." << endl;
+
+            break;
+        case 2:
+            ticks = clock(); // Initialize a timer variable before loading bids
+
+            bids = loadBids(csvPath); // method call to load the bids
+
+            ticks = clock() - ticks; // current clock ticks minus starting clock ticks
+            cout << "time: " << ticks << " milliseconds" << endl;
+            cout << "time: " << ticks*1.0/CLOCKS_PER_SEC << " seconds" << endl; // Calculate elapsed time and display result
+
+            break;
+        case 3:
+            for (int i = 0; i < bids.size(); ++i) { // Loop and display the bids read
+            	displayBid(bids.at(i));
+            }
+            cout << endl;
+
+            break;
+        }
+    }
+
+    cout << "Good bye." << endl;
+
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/Lab2_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2_1.dir/CSVparser.cpp.o"
+  "CMakeFiles/Lab2_1.dir/Lab2-1.cpp.o"
+  "Lab2_1"
+  "Lab2_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
